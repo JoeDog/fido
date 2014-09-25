5a671b20defbd5776d44d462da526a5129778f31
JoeDog <jeff@joedog.org>
2014-09-25 16:43:16 -0400

    Version increment 1.1.3

--- src/version.c
+++ src/version.c
@@ -4,11 +4,11 @@
  * used by configure to dynamically assign those values 
  * to documentation files.
  */
-const char *version_string = "1.1.1";
+const char *version_string = "1.1.3";
 const char *program_name   = "fido";
 const char *author_name    = "Jeffrey Fulmer, et al.";
 const char *email_address  = "jeff@joedog.org";
-const char *years          = "2000-2013";
+const char *years          = "2000-2014";
 const char *copyright      = "Copyright (C) 2011-2013 by Jeffrey Fulmer, et al.\n\
 This is free software; see the source for copying conditions.\n\
 There is NO warranty; not even for MERCHANTABILITY or FITNESS\n\
