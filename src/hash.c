7e9e8c71ff799f24acc4dcb9774774c47c1fcc8c
J. Fulmer <jdfulmer@lccdb029.armstrongceilings.com>
2020-05-27 09:59:50 -0400

    Added new includes

--- src/hash.c
+++ src/hash.c
@@ -24,10 +24,10 @@
 #endif/*HAVE_CONFIG_H*/
 
 #include <stdio.h>
+#include <stdarg.h>
 #include <string.h>
 #include <stdlib.h>
 #include <hash.h>
-#include <joedog/joedog.h>
 #include <joedog/defs.h>
 
 typedef struct NODE
@@ -304,7 +304,38 @@ hash_get_entries(HASH this)
 }
 
 /**
- * returns int cache key for the table
+ * Fowler/Noll/Vo hash
+ *
+ * The basis of this hash algorithm was taken from an idea sent
+ * as reviewer comments to the IEEE POSIX P1003.2 committee by:
+ *
+ * Phong Vo (http://www.research.att.com/info/kpv/)
+ * Glenn Fowler (http://www.research.att.com/~gsf/)
+ *
+ * In a subsequent ballot round:
+ * 
+ * Landon Curt Noll (http://www.isthe.com/chongo/)
+ *
+ * improved on their algorithm.  Some people tried this hash
+ * and found that it worked rather well.  In an EMail message
+ * to Landon, they named it the ``Fowler/Noll/Vo'' or FNV hash.
+ *
+ * FNV hashes are designed to be fast while maintaining a low
+ * collision rate. The FNV speed allows one to quickly hash lots
+ * of data while maintaining a reasonable collision rate.
+ *
+ * See: http://www.isthe.com/chongo/tech/comp/fnv/index.html
+ *
+ * This code is in the public domain.
+ *
+ * LANDON CURT NOLL DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO
+ * EVENT SHALL LANDON CURT NOLL BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF
+ * USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ *
  */
 private unsigned int
 fnv_32_buf(void *buf, size_t len, unsigned int hval) {
